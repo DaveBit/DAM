d16fedb55c6d1470b133a2ed8e10b94fcf685449
Dave! <60303855+DaveBit@users.noreply.github.com>
2022-06-04 19:11:05 +0200

    Create MiniCalculator.java

---
+++ DAM1/Programacion/Actividades/Unidad 10 Calculadora/MiniCalculator.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Unit10;
+
+import java.io.IOException;
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author David
+ */
+public class MiniCalculator extends Application {
+    
+    @Override
+    public void start(Stage primaryStage) throws IOException{
+        
+        Parent root = FXMLLoader.load(getClass().getResource("MiniCalculator.fxml"));
+        Scene scene = new Scene(root);
+        primaryStage.setTitle("Mini Calculadora JavaFX");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+            
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
